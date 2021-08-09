419d497efd03b72745c3d83fceb45fb8ad6ae962
Animeshxd <am2646374@gmail.com>
2021-08-09 21:05:35 +0530

    add element at certain position (self)

---
+++ SingleLinkedList/11_insertion_at_certain_position.c
@@ -0,0 +1,116 @@
+//add element at certain position
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define new malloc(sizeof(node))
+
+typedef struct node
+{
+	int data;
+	struct node *link;
+} node;
+
+node *append(node *current, int data);
+void append_(node * head, int data);
+int count(node *head);
+node *add_to_first(node *Node, int data);
+node *insert(node *head, int pos, int data);
+
+int main()
+{
+
+	node *head = new;
+	head->data = 1;
+	head->link = NULL;
+
+	node *current = head;
+	for (int i = 2; i < 11; i++)
+		current = append(current, i);
+		
+	printf("\nNew size of node %d \n", count(head));
+	head = insert(head, 1, 66);
+	printf("\nNew size of node %d \n", count(head));
+	node *ptr = head;
+	while (ptr != NULL)
+	{
+		printf(" %d ", ptr->data);
+		ptr = ptr->link;
+	}
+
+	return 0;
+}
+
+node *append(node *current, int data)
+{
+	node *new_node = new;
+	new_node->data = data;
+	new_node->link = NULL;
+
+	current->link = new_node;
+	return new_node;
+}
+void append_(node * head, int data){
+	
+	node *new_node = new;
+	new_node->data = data;
+	new_node->link = NULL;
+
+	node *ptr = head;
+	while (ptr->link != NULL)
+	{
+		ptr = ptr->link;
+	}
+	ptr->link = new_node;
+}
+
+int count(node *head)
+{
+	node *ptr = head;
+	int count = 0;
+	while (ptr != NULL)
+	{
+		++count;
+		ptr = ptr->link;
+	}
+	return count;
+}
+node *add_to_first(node *Node, int data)
+{
+	node *new_node = new;
+	new_node->data = data;
+
+	new_node->link = Node;
+	Node = new_node;
+	return new_node;
+}
+
+node *insert(node *head, int pos, int data)
+{
+	int total = count(head);
+	// printf(" %d \n", total);
+	if (total > pos && pos != 0)
+	{
+		node *ptr = head;
+		for (int i = 0; i < total; i++)
+		{
+			if (i == pos - 1)
+			{
+				printf("%d in pos %d\n", ptr->data, i);
+				printf("%d in pos %d\n", ptr->link->data, i + 1);
+				ptr->link = add_to_first(ptr->link, data);
+				return head;
+			}
+			ptr = ptr->link;
+		}
+	}
+	else if (pos == 0 && pos != total)
+	{
+		return add_to_first(head, data);
+	}
+	else
+	{
+		append_(head, data);
+		return head;
+	}
+}
