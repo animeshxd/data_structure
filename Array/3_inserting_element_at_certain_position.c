1c5ca2e30979b8f43c25f458e7dc9c344e47d800
Animeshxd <am2646374@gmail.com>
2021-08-11 23:52:01 +0530

    // Inserting Element at certain Position in array

---
+++ Array/3_inserting_element_at_certain_position.c
@@ -0,0 +1,25 @@
+// Inserting Element at certain Position in array
+
+#include <stdio.h>
+
+void insert(int, int);
+int main()
+{
+    int array[10];
+    for (int i = 0; i < 10; i++){
+        array[i] = i;
+    }
+
+    int arrayb[11];
+    
+        for (int i = 0; i < 10; i++){
+        arrayb[i] = array[i];
+    }
+
+    return 0;
+}
+
+void insert(int data, int pos){
+
+}
+
