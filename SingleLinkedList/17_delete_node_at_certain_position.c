e98cc121133a0c20c11c0512361db1aa78d94c63
Animesh Murmu <am2646374@gmail.com>
2021-08-16 01:20:37 +0530

    Single Linked List (Deleting the Node at a Particular Position)

--- SingleLinkedList/17_delete_node_at_certain_position.c
+++ SingleLinkedList/17_delete_node_at_certain_position.c
@@ -9,16 +9,32 @@ typedef struct node{
 
 node * create(int data);
 node * append(node *, int);
-node * delete(node * head, int pos, int data);
+node * delete(node * head, int pos);
 void print(node *);
 
 int main()
 {
-    
+    node * head = create(0);
+    node * current = head;
+
+    for (int i = 1; i < 10; i++) {
+	current = append(current, i);
+    }
+    print(head);
+
+    head = delete(head, 0);
+    print(head);
+
+    for (int i= 1; i<10; i++) {
+	head = delete(head, 1);
+	print(head);
+    }
+
     return 0;
 }
 
 node * create(int data){
+
     node * new_node = malloc(sizeof(node));
     new_node->data = data;
     new_node->link = NULL;
@@ -31,7 +47,7 @@ node * append(node * current, int data){
 }
 void print(node * head){
     node * ptr = head;
-    printf("");
+    printf("\n");
     while (ptr != NULL)
     {
         printf("%d ", ptr->data);
@@ -39,20 +55,35 @@ void print(node * head){
     }
     
 }
-node * delete(node * head, int pos, int data){
+node * delete(node * head, int pos){
     if (head == NULL){
-        printf("node is empty");
+        printf("\nnode is empty\n");
         return NULL;
     }else if (head->link == NULL){
-        printf("This it the Last Node");
+        printf("\nThis it the Last Node\n");
         free(head);
+	head = NULL;
         return head;
     }else{
 
+	if(pos == 0){
+	    free(head);
+	    head = head->link;
+	    return head;
+	}
+
         node * ptr = head;
+	int i = 0;
         while(ptr != NULL){
-            
-            
-        }
+	    if (i == pos -1){
+		free(ptr->link);
+		ptr->link = ptr->link->link;
+		return head;
+	    }
+	    ptr = ptr->link;
+	    i++;
+		
+	}
+	return head;
     }
 }
