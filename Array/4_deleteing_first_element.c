5fa4adbb5c13f46eea957e4449267521c3047926
Animeshxd <am2646374@gmail.com>
2021-08-15 19:01:43 +0530

    Deletion at the Beginning

---
+++ Array/4_deleteing_first_element.c
@@ -0,0 +1,14 @@
+// Deletion at the Beginning in array
+#include <stdio.h>
+
+int main()
+{
+    int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < 9; i++){
+        array[i]  = array[i+1];
+    }
+    array[10] = 0;
+    for (int i = 0; i < 9; i++)
+    printf("%d ", array[i]);
+
+}
